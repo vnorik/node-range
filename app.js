8369ce57c22686173e3787a2c13011bc0ee81927
vnorik <norik1988@gmail.com>
2013-07-07 15:27:10 +0300

    Added layout

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var app = express();
 var pages = require(__dirname + '/controllers/pages');
+var ejsLocals = require('ejs-locals');
 
 //configuration settings
+app.engine('ejs', ejsLocals);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'));
 
+//mount routes
 app.get('/', function (req, res) {
     res.redirect('home');
 });
-
 app.get('/home', pages.home);
+app.get('/about', pages.about);
 
 module.exports = app;
--- controllers/pages.js
+++ controllers/pages.js
@@ -4,3 +4,10 @@ exports.home = function (req, res) {
         message: 'This is the "home" action of "pages" controller'
     });
 }
+
+exports.about = function (req, res) {
+    res.render('pages/about', {
+        title: 'About page',
+        message: 'This is the "about" action of "pages" controller'
+    });
+}
